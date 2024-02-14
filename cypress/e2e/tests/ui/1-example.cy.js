58dad1adf4e149335e20f5dc00488302e749826b
Alex Polukhin <alex@mistho.io>
2024-02-14 15:02:20 +0100

    update for page 0 adn 1

--- cypress/e2e/tests/ui/1-example.cy.js
+++ cypress/e2e/tests/ui/1-example.cy.js
@@ -13,31 +13,44 @@ describe('First page cases', () => {
 
     })
 
-    it('Page display with correct text', () => {
-
-        cy.get('h1')
-            .should('be.visible')
-            .and('have.text', FIXTURE.title)
-        // text display in center
-        cy.get('.sc-bdVaJa')
-            .should('have.css', 'justify-content')
-            .and('eq', 'center')
-        // text display in white
-        cy.get('.sc-bdVaJa')
-            .should('have.css', 'color')
-            .and('eq', 'rgb(255, 255, 255)')
-        // text display in correct font-size
-        cy.get('.sc-bdVaJa')
-            .should('have.css', 'font-size')
-            .and('eq', '32px')
-
-    })
-
     it('Page display with correct background', () => {
 
         cy.get('.sc-bdVaJa')
             .should('have.css', 'background-color')
             .and('eq', 'rgb(14, 118, 168)')
             
-    });
+    })
+
+    it('text is correct', () => {
+
+        cy.get('h1')
+            .should('be.visible')
+            .and('have.text', FIXTURE.title)
+
+    })
+
+    it('text display in center', () => {
+
+        cy.get('.sc-bdVaJa')
+            .should('have.css', 'justify-content')
+            .and('eq', 'center')
+
+    })
+
+    it('text in white', () => {
+
+        cy.get('.sc-bdVaJa')
+            .should('have.css', 'color')
+            .and('eq', 'rgb(255, 255, 255)')
+
+    })
+
+    it('text has correct font-size', () => {
+
+        cy.get('.sc-bdVaJa')
+            .should('have.css', 'font-size')
+            .and('eq', '32px')
+
+    })
+
 })
